b2abdb03aca4a17a2c6f30bc35d7f2c4d14ad37a
Jan Hrncir <jan.hrncir.pe@gmail.com>
2023-04-04 18:25:16 +0200

    aStar for chaotic warehouse

---
+++ optimizer.py
@@ -0,0 +1,116 @@
+from warehouse import Warehouse, get_random_orders
+from queue import PriorityQueue
+import time
+
+class WarehouseHeuristic(Warehouse):
+	def __init__(self, num_box=5, num_stack=3, order=None):
+		super().__init__(num_box, num_stack, order)
+	
+	def noHeuristic(self):
+		return 0
+
+	def find_pos_in_stack(self, num, warehouse):
+		for i,stack in enumerate(warehouse):
+			for j,val in enumerate(stack):
+				if val == num:
+					return j + 1 # ret num of box above finded box
+		return 0
+
+	def heuristic(self):
+		curr_state = self.get_state()
+		goal_order = self.get_goal_out()
+
+		heur = 0
+		for goal_val in goal_order:
+			heur += self.find_pos_in_stack(goal_val,curr_state)
+			
+		return heur
+
+
+class State():
+	def __init__(self, warehouse, cost = 0, priority = 0, history = (None, None)):
+		self.warehouse = warehouse
+		self.cost = cost # number of moves
+		self.priority = priority
+		self.history = history
+
+	def copy(self):
+		return State(self.warehouse.clone(), self.cost, self.priority, self.history)
+
+	def __lt__(self, other): # Overrides "<" operator, needed in PriorityQueue.
+		return self.priority < other.priority
+	def __str__(self):
+		return f"{self.warehouse} action: {self.history[0]} prev: {self.history[1]} cost: {self.cost} priority: {self.priority}"
+
+class AStar():
+	def search(self, start):
+		# Return a list of optimal actions that takes start to goal.
+
+		opened = PriorityQueue()
+		closed = {}
+		state = State(start.clone())
+		opened.put(state)
+	
+		while not opened.empty():
+			state = opened.get()
+			#print(state)
+			action, prev_state = state.history
+			if state.warehouse.isDone():
+				#return path
+				path = [action]
+				while prev_state is not None:
+					action, prev_state = closed[prev_state]
+					if action is not None:
+						path.append(action)
+				return list(reversed(path))
+			if state.warehouse in closed:
+				continue
+			else:
+				closed[state.warehouse] = (action, prev_state)
+			
+			for action, neighbor in state.warehouse.get_neighbors():
+				next_state = State(neighbor, state.cost+1, 0, (action, state.warehouse))
+				next_state.priority = next_state.cost + 1.1 * next_state.warehouse.heuristic()
+				opened.put(next_state)
+
+		return None
+	
+
+if __name__ == "__main__":
+	
+	#N=1000 S=4 Total expanded nodes: 10744 Time: 80.24
+
+	N = 16 #number of box in warehouse
+	S = 3 #size of warehouse (num_stack)
+	
+	order = (2,1) #sequence of box to go out 
+
+	rnd_order = get_random_orders(N,S)
+
+	start = WarehouseHeuristic(N,S, rnd_order)
+
+	print(f"Searching path: {start} -> for order {start.get_goal_out()}")
+
+	astar = AStar()
+
+	start_t = time.time()
+	path = astar.search(start)
+	end_t = time.time()
+	elapsed_t = end_t - start_t
+
+	if path is not None:
+		print(f"Found a path (length={len(path)}): ")
+		print(path)
+	    
+		print("How it goes: ")
+
+		s = start.clone()
+		print(s)
+		for a in path:
+			s.apply(a)
+			print(s)    
+	else:
+		print("NO PATH exists.")
+	
+	print(f"Total expanded nodes: {Warehouse.expanded} Time: {elapsed_t:.2f}")
+
---
+++ warehouse.py
@@ -0,0 +1,173 @@
+import random, copy, numpy as np
+
+def get_random_orders(num_obj, num_orders):
+     obj = np.arange(1, num_obj+1)
+     return tuple(np.random.choice(obj,num_orders, replace=False))
+
+
+def _get_random_state(num_obj, num_stack):
+    stacks = []
+    obj = np.arange(1, num_obj+1)
+
+    while len(obj) > 0:
+        stack_len = np.random.randint(1, len(obj)+1)
+        stack = np.random.choice(obj, stack_len, replace=False)
+
+        if len(stacks) < num_stack:
+            stacks.append(stack)
+        else:
+            for o in stack:
+                rand_stack_id = np.random.randint(0,num_stack)
+                stacks[rand_stack_id] = np.insert(stacks[rand_stack_id],0,o)
+
+        obj = np.setdiff1d(obj, stack)
+
+    # add empty stack to assure full warehouse
+    if len(stacks) < num_stack: 
+         for _ in range(num_stack - len(stacks)):
+              empty = np.empty(0, dtype=np.int32)
+              stacks.append(empty)
+
+    return stacks
+
+def _find_stack(stacks, item):
+	for stack_id, stack in enumerate(stacks):
+		if stack.size != 0 and stack[0] == item:
+			return stack, stack_id
+
+	return None, None
+
+def _find_empty_stack(stacks):
+	for stack_id, stack in enumerate(stacks):
+		if stack.size == 0:
+			return stack, stack_id
+
+	return None, None
+
+class Warehouse():
+    expanded = 0
+
+    def __init__(self, num_box=5, num_stack=3, order=None) -> None:
+         #create random state
+          self.state = _get_random_state(num_box, num_stack)
+          self.out = np.empty(0,dtype=np.int32)
+          self.order = order
+          self.stateFset = frozenset(tuple(o) for o in self.state)
+
+
+    def apply(self, action):
+         what, where = action
+
+         if what==where:
+              print("!invalid action what==where")
+              return
+         
+         stack_from, stack_from_id = _find_stack(self.state, what)
+         if stack_from is None:
+              print("!invalid action cannot move what")
+              return
+         
+         if where == 0: # to ground of empty stack
+              stack_to, stack_to_id = _find_empty_stack(self.state)
+         
+         elif where == -1: # to ground of output stack
+              stack_to, stack_to_id = self.out, -1
+         else:
+              stack_to, stack_to_id = _find_stack(self.state, where)     
+        
+         if stack_to is None:
+                print("!invalid action empty stack or where DON'T exists")
+                return
+
+         #move object
+         self.state[stack_from_id] = np.delete(stack_from,0)
+         if stack_to_id != -1:
+            self.state[stack_to_id] = np.insert(stack_to,0, what)
+         else:
+            self.out = np.insert(self.out,0,what)
+
+         self.stateFset = frozenset(tuple(o) for o in self.state)
+
+    def get_action(self):
+          Warehouse.expanded += 1
+
+          actions = []
+          for s_from in self.state:
+                if s_from.size == 0:
+                      continue
+                object_a = s_from[0]
+
+                if self.out.size < len(self.order) and object_a == self.order[-self.out.size - 1]: #obj_a is next ordered box
+                      actions.append((object_a, -1)) # obj a move to output
+
+                for s_to in self.state:
+                      if len(s_from) > 1 and s_to.size == 0:
+                            actions.append((object_a,0)) # obj a move to ground
+                            continue
+                      elif s_to.size == 0:
+                            continue
+
+                      object_b = s_to[0]
+
+                      if object_a != object_b:
+                            actions.append((object_a, object_b))
+
+          return actions
+    
+    def get_neighbors(self):
+          neighbors = []
+
+          for a in self.get_action():
+                new_state = self.clone()
+                new_state.apply(a)
+
+                neighbors.append((a,new_state))
+          
+          return neighbors
+    
+    def get_state(self):
+         return self.stateFset
+
+    def get_cur_out(self):
+         return tuple(self.out)
+    
+    def get_goal_out(self):
+         return self.order
+    
+    def isDone(self):
+         return self.get_cur_out() == self.get_goal_out()
+
+    def __str__(self) -> str:
+        return str([list(o) for o in self.state]) + " Output: " +str(self.out) + " Orders: " +str(self.order) + " Done: " + str(self.isDone())
+    
+    def clone(self):
+        warehouse = type(self)(0)
+        warehouse.state = copy.deepcopy(self.state)
+        warehouse.out = copy.deepcopy(self.out)
+        warehouse.order = self.order
+
+        return warehouse
+          
+    
+
+if __name__ == "__main__":
+
+    # rndState = _get_random_state(6,3)
+    # print(rndState)
+    # print(frozenset(tuple(o) for o in rndState))
+    N = 6 #number of box in warehouse
+    S = 3 #size of warehouse (num_stack)
+
+    order = get_random_orders(N,S)
+    wh = Warehouse(N,S, order)
+    #cwh = wh.clone()
+    
+
+    while True:
+          #print(cwh)
+          print(f"state: {wh}")
+          print(f"actions = {wh.get_action()}")
+          print("<from> <to>: ", end="")
+        
+          o_from, o_to = [int(x) for x in input().split()]
+          wh.apply((o_from,o_to)) 
